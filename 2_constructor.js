c16a42d80a9f704a13f4969600bf88dfe039c2de
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-01-11 21:15:54 -0500

    oop crash course

---
+++ 2_constructor.js
@@ -0,0 +1,16 @@
+// Constructor (ES5)
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+
+  this.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+}
+
+// Instatiate an Object
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Poe', 2016);
+
+console.log(book2);
